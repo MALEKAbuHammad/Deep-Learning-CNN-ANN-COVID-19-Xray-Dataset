76003841318b63238fc4e916b158a126477b87ff
Malek AbuHammad <abuhammadmalek4@gmail.com>
2025-04-06 20:58:10 +0300

    Update file: describe the changes made

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ uploaded_file = st.file_uploader("📂 Upload an X-ray image", type=["jpg", "png
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
-    st.image(img, caption='📷 Uploaded Image', use_column_width=True)
+    st.image(img, caption='📷 Uploaded Image', use_container_width=True)
 
     img = img.convert("RGB") 
     img = img.resize((224, 224))  
